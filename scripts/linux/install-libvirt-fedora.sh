5fbab4f3360bb255c34ef1e3ce71c89e1ddc08c3
Brian Cain <bcain@hashicorp.com>
2020-04-07 12:04:11 -0700

    Add missing install command for dnf

--- scripts/linux/install-libvirt-fedora.sh
+++ scripts/linux/install-libvirt-fedora.sh
@@ -1,1 +1,1 @@
-sudo dnf -y bridge-utils libvirt virt-install qemu-kvm make libxslt-devel libxml2-devel libvirt-devel libguestfs-tools-c ruby-devel gcc virt-top libguestfs-tools
+sudo dnf -y install bridge-utils libvirt virt-install qemu-kvm make libxslt-devel libxml2-devel libvirt-devel libguestfs-tools-c ruby-devel gcc virt-top libguestfs-tools
